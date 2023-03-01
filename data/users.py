346d556c07b7a0f83367a3b58067810cfe953cff
Ivanova Polina <ivanova.p@mail.ru>
2023-03-01 18:59:46 +0300

    yoy

--- data/users.py
+++ data/users.py
@@ -1,5 +1,7 @@
 import datetime
 import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
 
@@ -16,7 +18,11 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
-    news = orm.relationship("News", back_populates='user')
 
-    def __repr__(self) -> str:
-        return f"<User> ${self.id} ${self.name} ${self.email}"
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    news = orm.relationship("News", back_populates='user')
